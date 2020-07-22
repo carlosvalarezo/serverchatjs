3868f9634c0102071404c4728021a0f0c2e74c7e
carlosvalarezo <carlos.valarezo@gmail.com>
2020-07-22 13:40:38 -0500

    [@carlosvalarezo] updates uris for local and remote connections

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = process.env.DEPLOY ? config.get('mongoREMOTE') : config.get("mongoLOCAL");
+const db = process.env.DEPLOY ? config.get('mongoREMOTE') : config.get('mongoLOCAL');
 
 const connectDB = async () => {
   const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true};
   try{
+    console.log(`mongodb should connect to ${db}`);
     await mongoose.connect(db, options);
     console.log('mongodb connected...');
   }
